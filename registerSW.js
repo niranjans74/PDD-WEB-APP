d745b130e4bf28f504d3b97a31f95109c308ebed
niranjans74 <niranjanniranjan0047@gmail.com>
2026-06-23 10:41:48 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/PDD-WEB-APP/sw.js', { scope: '/PDD-WEB-APP/' })})}
